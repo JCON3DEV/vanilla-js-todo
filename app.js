d6c3ca15e522b9e8e6c0c358154921dec5aefe06
Jordan <jconheasman@gmail.com>
2021-01-29 15:44:27 -0800

    completed function to sort the different states of the todo list.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(event){
@@ -70,4 +72,41 @@ function deleteCheck(e){
     todo.classList.toggle('completed')
 
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  // console.log(todos);
+  // This is the wizzardry;
+  todos.forEach(function(todo){
+    // e.target.value will be either; all / completed / uncompleted
+    console.log("this it todo", todo);
+    if (todo.classList !== undefined) {
+
+      switch(e.target.value){
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if(todo.classList.contains('completed')){
+            todo.style.display = "flex";
+          } else{
+            todo.style.display = "none"
+          }
+          break;
+        case "uncompleted":
+          if(!todo.classList.contains('completed')){
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "default":
+          todo.style.display = "none";
+          break;
+      }
+    }
+  })
+
+
 }
